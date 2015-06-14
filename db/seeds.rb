18d5f886245edaf90897c6986baa914040090a66
Konrad Marzec <kmarzec07@gmail.com>
2015-06-14 18:45:43 +0200

    seeds user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+u = User.create(:email => "user@name.com", :password => 'password', :password_confirmation => 'password', admin: true)
