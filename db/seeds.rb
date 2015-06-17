abfe0eee25e0cdf84d96cc2c9fd89ce6cbb8fc25
Konrad Marzec <kmarzec07@gmail.com>
2015-06-17 23:04:57 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,98 @@ Category.create!([
                      {name: 'Life'},
                      {name: 'Work'}
                  ])
+
+Product.create!([
+                    {title: 'Cake', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo
+                                                  ut eros consequat gravida et non mauris. Curabitur ac leo odio. Fusce
+                                                  faucibus est sit amet viverra malesuada. Phasellus efficitur ullamcorper
+                                                  magna, a vestibulum lacus semper id. Donec rutrum sit amet nunc eu
+                                                  endrerit. Etiam vitae mauris sed tortor aliquet porttitor.', price: 213,
+                     category_id: 1, user_id: 1},
+                    {title: 'Fish', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo
+                                                  ut eros consequat gravida et non mauris. Curabitur ac leo odio. Fusce
+                                                  faucibus est sit amet viverra malesuada. Phasellus efficitur ullamcorper
+                                                  magna, a vestibulum lacus semper id. Donec rutrum sit amet nunc eu
+                                                  endrerit. Etiam vitae mauris sed tortor aliquet porttitor.', price: 123,
+                     category_id: 1, user_id: 2},
+                    {title: 'Carrot', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo
+                                                  ut eros consequat gravida et non mauris. Curabitur ac leo odio. Fusce
+                                                  faucibus est sit amet viverra malesuada. Phasellus efficitur ullamcorper
+                                                  magna, a vestibulum lacus semper id. Donec rutrum sit amet nunc eu
+                                                  endrerit. Etiam vitae mauris sed tortor aliquet porttitor.', price: 23,
+                    category_id: 1, user_id: 1},
+
+                    {title: 'Game', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo
+                                                  ut eros consequat gravida et non mauris. Curabitur ac leo odio.
+                                                  magna, a vestibulum lacus semper id. Donec rutrum sit amet nunc eu
+                                                  endrerit. Etiam vitae mauris sed tortor aliquet porttitor.', price: 213,
+                     category_id: 2, user_id: 1},
+                    {title: 'Football', description: 'Curabitur ac leo odio. Fusce
+                                                  faucibus est sit amet viverra malesuada. Phasellus efficitur ullamcorper
+                                                  magna, a vestibulum lacus semper id. Donec rutrum sit amet nunc eu
+                                                  endrerit. Etiam vitae mauris sed tortor aliquet porttitor.', price: 123,
+                     category_id: 2, user_id: 3},
+
+                    {title: 'Health', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo
+                                                  ut eros consequat gravida et non mauris. Curabitur ac leo odio. Fusce
+                                                  faucibus est sit amet viverra malesuada. Phasellus efficitur ullamcorper
+                                                  magna, a vestibulum lacus semper id.', price: 23,
+                     category_id: 3, user_id: 4}
+                ])
+
+Review.create!([
+                   {content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo
+                              ut eros consequat gravida et non mauris. Curabitur ac leo odio. Fusce
+                              faucibus est sit amet viverra malesuada. Phasellus efficitur ullamcorper
+                              magna, a vestibulum lacus semper id.',
+                   rating: 5, product_id: 1, user_id: 1
+                   },
+                   {content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo
+                              ut eros consequat gravida et non mauris. Curabitur ac leo odio. Fusce
+                              faucibus est sit amet viverra malesuada. Phasellus efficitur ullamcorper
+                              magna, a vestibulum lacus semper id.',
+                    rating: 3, product_id: 1, user_id: 3
+                   },
+                   {content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo
+                              ut eros consequat gravida et non mauris. Curabitur ac leo odio. Fusce
+                              faucibus est sit amet viverra malesuada. Phasellus efficitur ullamcorper
+                              magna, a vestibulum lacus semper id.',
+                    rating: 5, product_id: 2, user_id: 1
+                   },
+                   {content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo
+                              ut eros consequat gravida et non mauris. Curabitur ac leo odio. Fusce
+                              faucibus est sit amet viverra malesuada. Phasellus efficitur ullamcorper
+                              magna, a vestibulum lacus semper id.',
+                    rating: 3, product_id: 2, user_id: 4
+                   },
+                   {content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo
+                              ut eros consequat gravida et non mauris. Curabitur ac leo odio. Fusce
+                              faucibus est sit amet viverra malesuada. Phasellus efficitur ullamcorper
+                              magna, a vestibulum lacus semper id.',
+                    rating: 5, product_id: 5, user_id: 2
+                   },
+                   {content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo
+                              ut eros consequat gravida et non mauris. Curabitur ac leo odio. Fusce
+                              faucibus est sit amet viverra malesuada. Phasellus efficitur ullamcorper
+                              magna, a vestibulum lacus semper id.',
+                    rating: 1, product_id: 1, user_id: 1
+                   },
+                   {content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo
+                              ut eros consequat gravida et non mauris. Curabitur ac leo odio. Fusce
+                              faucibus est sit amet viverra malesuada. Phasellus efficitur ullamcorper
+                              magna, a vestibulum lacus semper id.',
+                    rating: 5, product_id: 1, user_id: 3
+                   },
+                   {content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo
+                              ut eros consequat gravida et non mauris. Curabitur ac leo odio. Fusce
+                              faucibus est sit amet viverra malesuada. Phasellus efficitur ullamcorper
+                              magna, a vestibulum lacus semper id.',
+                    rating: 2, product_id: 3, user_id: 2
+                   },
+                   {content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu leo
+                              ut eros consequat gravida et non mauris. Curabitur ac leo odio. Fusce
+                              faucibus est sit amet viverra malesuada. Phasellus efficitur ullamcorper
+                              magna, a vestibulum lacus semper id.',
+                    rating: 2, product_id: 6, user_id: 4
+                   }
+               ])
